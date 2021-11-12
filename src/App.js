cb2818a7ff1fc6a220a63b8edc1969e6c36bfa7c
Kévin <kevin.joya@outlook.com>
2021-11-12 18:50:36 +0100

    Added compos sigin in, up, cockttails

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { createGlobalStyle } from "styled-components";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import SigninSpace from './components/SigninSpace';
+import { ThemeProvider } from 'styled-components';
+import variables  from './variables';
+import SignUpSpace from './components/SignupSpace';
+import Cocktails from './components/Cocktails/Cocktails';
+
 
 function App() {
+  const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: white;
+  };
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+      <ThemeProvider theme={{ ...variables}}>
+        <GlobalStyle></GlobalStyle>
+          <>
+          <Routes>
+            <Route exact path='/' element={<SigninSpace />}></Route>
+            <Route exact path='/inscription' element={<SignUpSpace />}></Route>
+            <Route exact path='/cocktails' element={<Cocktails />}></Route>
+          </Routes>
+          </>
+        </ThemeProvider>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
