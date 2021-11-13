28d5381a5417600a33551c58b28e220d5bb9c9c9
Kévin <kevin.joya@outlook.com>
2021-11-13 19:54:52 +0100

    Added logic for cards & modal

--- src/components/Cocktails/Cocktails.js
+++ src/components/Cocktails/Cocktails.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import Card from "../Card/Card";
+import Header from "../Header/Header";
+import CocktailsStyled from "../../style/CocktailsStyled.style";
+import Modal from "../Modal/Modal";
 
 function Cocktails() {
   const [data, setData] = useState({
     cocktails: [],
     error: {},
-    errorMessage: ""
+    errorMessage: "",
+    isLogout: false,
+    modalLogout: {
+      title: 'Déconnexion',
+      content: 'Êtes-vous sûr de vouloir vous déconnecter ?',
+      btnCancel: 'Annuler',
+      btnConfirm: 'Se déconnecter'
+    }
   });
 
   React.useEffect(() => {
@@ -29,9 +40,39 @@ function Cocktails() {
     }
   }
 
+  function goLogout() {
+    const newState = { ...data};
+
+    newState.isLogout = !newState.isLogout;
+    return setData(newState);
+  }
 
   return (
     <React.Fragment>
+      <section>
+        <Header logout={() => goLogout()}></Header>
+        {data.isLogout &&
+          <Modal
+            modal={data.modalLogout}
+            cancelAction={() => goLogout()}
+          >
+          </Modal>
+        }
+
+        <CocktailsStyled className='container-d-fluid col-12'>
+          <div className="section__title">
+            <h2>Carte des cocktails</h2>
+            <button type="button" className="section__title--btn">Ajoute ton cocktail</button>
+          </div>
+          <div className="row">
+            {
+              data.cocktails.map((card, index) => {
+                return <Card card={card} key={index}></Card>
+              })
+            }
+          </div>
+        </CocktailsStyled>
+      </section>
     </React.Fragment>
   )
 }
