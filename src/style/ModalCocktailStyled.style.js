f1bc7376b4a090fca8711630d7ea50c2d98b65e2
Kévin <kevin.joya@outlook.com>
2021-11-14 15:57:25 +0100

    Changed some style

--- src/style/ModalCocktailStyled.style.js
+++ src/style/ModalCocktailStyled.style.js
@@ -15,15 +15,71 @@ const ModalCocktailStyled = styled.div`
 
   .modal-content {
     background-color: #fefefe;
-    margin: 15% auto;
+    margin: 0 auto;
+    margin-top: 5%;
     padding: 20px;
     border: 1px solid #888;
-    width: 80%;
+    width: 80%;;
+
+    &__title {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &__ingredients {
+      margin: 15px 0;
+      h4 {
+        font-size: 1.1em;
+        font-family: ${props => props.theme.fontFamilyText};
+        font-weight: bold;
+      }
+      ul {
+        button {
+          background-color: ${props => props.theme.colorBtn};
+          color: ${props => props.theme.colorPrincipal};
+          border: 1px solid ${props => props.theme.colorBtn};
+          padding: 10px;
+          border-radius: 5px;
+          margin-bottom: 10px;
+        }
+        li {
+          list-style-type: none;
+
+          input {
+            width: 25%;
+          }
+        }
+      }
+    }
+
+    &__recipe {
+      h4 {
+        font-size: 1.1em;
+        font-family: ${props => props.theme.fontFamilyText};
+        font-weight: bold;
+      }
+    }
+
+    input {
+      border: none;
+      border-bottom: 1px solid ${props => props.theme.colorLola};
+      width: 35%;
+      margin: 10px 0;
+    }
+
+    textarea {
+      border: 1px solid ${props => props.theme.colorLola};
+      width: 50%;
+      height: 15vh;
+    }
   }
 
-  .modal-content__title {
-    display: flex;
-    justify-content: space-between;
+  .modal-footer {
+    justify-content: space-around;
+    button {
+      font-size: 1.2em;
+      width: 30%;
+    }
   }
 
   .close {
