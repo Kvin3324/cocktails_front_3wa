c1f1a1b8168602f915b04bc5d6fc45f5ee75ffb9
Kévin <kevin.joya@outlook.com>
2021-11-15 18:05:28 +0100

    Added edit icon

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import CardStyled from '../../style/CardStyled.style';
+import React from "react";
+import CardStyled from "../../style/CardStyled.style";
 import { Link } from "react-router-dom";
 
 function Card(props) {
   return (
     <React.Fragment>
       <CardStyled as="div" className="mb-4 ml-2 col-4">
-          <Link to={ `/cocktails/${props.card._id}`}  onClick={(e) => e.stopPropagation()}>
+        <Link
+          to={`/cocktails/${props.card._id}`}
+          onClick={(e) => e.stopPropagation()}
+        >
           <div className="card-product">
-          <span className="close" onClick={(e) => props.deleteCard(e)}>&times;</span>
-            <img src={props.card.img} alt="card--img"/>
+            <img
+              src="https://img.icons8.com/material-outlined/24/000000/edit--v1.png"
+              className="card-product_edit-icon"
+              alt="edit-icon"
+              onClick={(e) => props.editCard(e)}
+            />
+            <span className="close" onClick={(e) => props.deleteCard(e)}>
+              &times;
+            </span>
+            <img src={props.card.img} alt="card--img" />
             <div className="card-product-infos">
               <h2>{props.card.name}</h2>
               <h3>Description</h3>
@@ -19,7 +30,7 @@ function Card(props) {
         </Link>
       </CardStyled>
     </React.Fragment>
-  )
+  );
 }
 
 export default Card;
