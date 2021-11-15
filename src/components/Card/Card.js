3a99ab79966d5195026028bb545c92022d09bc12
Kévin <kevin.joya@outlook.com>
2021-11-15 14:53:09 +0100

    Added close arrow

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 import CardStyled from '../../style/CardStyled.style';
 import { Link } from "react-router-dom";
 
-
 function Card(props) {
   return (
     <React.Fragment>
       <CardStyled as="div" className="mb-4 ml-2 col-4">
-          <Link to={`/cocktails/${props.card._id}`}>
+          <Link to={ `/cocktails/${props.card._id}`}  onClick={(e) => e.stopPropagation()}>
           <div className="card-product">
+          <span className="close" onClick={(e) => props.deleteCard(e)}>&times;</span>
             <img src={props.card.img} alt="card--img"/>
             <div className="card-product-infos">
               <h2>{props.card.name}</h2>
@@ -22,4 +22,4 @@ function Card(props) {
   )
 }
 
-export default Card
+export default Card;
