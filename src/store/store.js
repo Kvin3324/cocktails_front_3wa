45854a6b173c67047a7aa7c8bfa47b82c458e060
Kévin <kevin.joya@outlook.com>
2021-11-15 14:54:10 +0100

    Created store

---
+++ src/store/store.js
@@ -0,0 +1,32 @@
+import {createStore, combineReducers} from "redux";
+
+const store = {
+  cocktailsData: {
+    cocktails: [],
+    cocktail: null
+  },
+  userData: {
+    profile: null,
+  },
+};
+
+function cocktailReducer(state = store.cocktailsData, action) {
+  const newState = {...state};
+
+  if (action.type === "ADD_COCKTAILS") {
+    newState.cocktails = action.cocktails;
+
+    return newState;
+  }
+
+  console.log(action.type);
+  if (action.type === 'ADD_COCKTAIL') {
+    newState.cocktail = action.cocktail;
+
+    return newState;
+  }
+
+  return newState;
+}
+
+export default createStore(combineReducers({cocktailReducer}))
