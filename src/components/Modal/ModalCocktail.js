1377b894572dcf44e531933e7c5d98f7d497d9b4
Kévin <kevin.joya@outlook.com>
2021-11-17 14:32:48 +0100

    Added success toast when created

--- src/components/Modal/ModalCocktail.js
+++ src/components/Modal/ModalCocktail.js
@@ -69,6 +69,16 @@ function ModalCocktail(props) {
         errorMessage: error.message,
       });
     }
+
+    toast.success('Cocktail créé au monde entier !', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   }
 
   return (
